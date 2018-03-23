fe39df834100cebf3dd2bb53b92e65ef6bf95301
Katryn McIntosh <katrynmcintosh@swipely.com>
2018-03-22 21:04:29 -0400

    Add necessary type libs

--- src/spot.ts
+++ src/spot.ts
@@ -1,3 +1,11 @@
+import dotenv from "dotenv";
+import { map, extend } from "lodash";
+import fs from "fs";
+import ps from "child_process";
+import temp from "temp";
+import moment from "moment";
+import request from "request";
+import blessed from "blessed";
 function greeter(person: string): string {
   return "Oh hai, " + person;
 }
