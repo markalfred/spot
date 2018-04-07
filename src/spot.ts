670292564e1bbd441e430ddf92d66a3de5a71687
Katryn McIntosh <katrynmcintosh@swipely.com>
2018-04-07 19:07:55 -0400

    Render a list of user's playlists

--- src/spot.ts
+++ src/spot.ts
@@ -1,6 +1,8 @@
 require("dotenv").load();
-import temp from "temp";
-import request from "request";
+import * as temp from "temp";
+import * as request from "request";
+import * as blessed from "blessed";
+import * as _ from "lodash";
 
 interface IRequestOptsHeaders {
   Authorization: string;
@@ -13,6 +15,7 @@ interface IRequestOpts {
 let REQUEST_OPTS: IRequestOpts | null = null;
 let IMG_DIR: string | null = null;
 let SOUND_DIR: string | null = null;
+let AUTH_TOKEN: string | null = null;
 
 const setup = (): void => {
   getToken();
@@ -25,6 +28,11 @@ const setup = (): void => {
   });
 };
 
+const render = (): void => {
+  playlists.focus();
+  screen.render();
+};
+
 interface IRequestForm {
   grant_type: string;
 }
@@ -33,7 +41,6 @@ interface IRequestForm {
 interface IRequestOptions {
   url: string;
   headers: IRequestOptsHeaders;
-  json: boolean;
 }
 
 interface IAuthOptions extends IRequestOptions {
@@ -50,23 +57,98 @@ const getToken = (): void => {
     },
     form: {
       grant_type: "client_credentials"
-    },
-    json: true
+    }
   };
 
   request.post(authOptions, function(error, response, body) {
     if (!error && response.statusCode === 200) {
-      const token: string = body.access_token;
-      const options: IRequestOptions = {
-        url: `https://api.spotify.com/v1/users/${process.env.USERNAME}`,
-        headers: {
-          Authorization: `Bearer ${token}`
-        },
-        json: true
-      };
-      request.get(options, function(error, response, body) {
-        console.log(body);
-      });
+      AUTH_TOKEN = JSON.parse(body).access_token;
+      REQUEST_OPTS = { headers: { Authorization: `Bearer ${AUTH_TOKEN}` } };
+      render();
+      getPlaylists();
     }
   });
 };
+
+const getPlaylists = () => {
+  const options: IRequestOptions = {
+    url: `https://api.spotify.com/v1/users/${
+      process.env.USERNAME
+    }/playlists?limit=50`,
+    headers: {
+      Authorization: `Bearer ${AUTH_TOKEN}`
+    }
+  };
+
+  request.get(options, function(error, response, body) {
+    if (error) {
+      console.log(error);
+    }
+    const playlistJson = JSON.parse(body);
+
+    playlists.setItems(_.map(playlistJson.items, "name"));
+    screen.render();
+  });
+};
+
+var screen = blessed.screen({
+  smartCSR: true,
+  dockBorders: true
+});
+
+screen.title = "Spot";
+
+const title = blessed.text({
+  parent: screen,
+  top: "top",
+  left: "center",
+  align: "center",
+  height: 1,
+  style: {
+    fg: "green"
+  },
+  tags: true,
+  content: "{bold} Spot {/}"
+});
+
+const sidebar = blessed.box({
+  parent: screen,
+  top: title.height,
+  left: 0,
+  height: `100%-${title.height}`,
+  border: "line"
+});
+
+const maxWidth = 40;
+if (sidebar.width > maxWidth) {
+  sidebar.width = maxWidth;
+}
+
+const playlists = blessed.list({
+  parent: sidebar,
+  top: 0,
+  left: 0,
+  height: "shrink",
+  width: "100%-2",
+  scrollable: true,
+  mouse: true,
+  keys: true,
+  vi: true,
+  content: "Loading...",
+  padding: {
+    top: 0,
+    right: 1,
+    bottom: 0,
+    left: 1
+  },
+  style: {
+    item: { fg: "grey" }
+  },
+  scrollbar: { ch: " " }
+});
+
+setup();
+
+screen.key("q", () => {
+  process.exit(0);
+});
